64163bc0532285817099d6ff5bcec0f1072ed8a9
Romario <boriso-rom@yandex.ru>
2023-11-29 15:36:46 +0300

    added method render color in class map

--- TicTacToeGame/Map.java
+++ TicTacToeGame/Map.java
@@ -4,10 +4,37 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Map extends JPanel {
-    Map() {
-        setBackground(Color.BLACK);
-    }
+    private int panelWidth;
+    private int panelHeigth;
+    private int cellHeigth;
+    private int cellWidth;
+    Map() {}
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d\nWin Length: %d", mode, fSzX, fSzY, wLen);
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g){
+
+        panelWidth = getWidth();
+        panelHeigth = getHeight();
+        cellHeigth = panelHeigth / 3;
+        cellWidth = panelWidth / 3;
+
+        g.setColor(Color.BLACK);
+        for (int h = 0; h < 3; h++) {
+            int y = h * cellHeigth;
+            g.drawLine(0, y, panelWidth, y);
+        }
+        for (int w = 0; w < 3; w++) {
+            int x = w * cellWidth;
+            g.drawLine(x, 0, x, panelHeigth);
+        }
     }
 }
