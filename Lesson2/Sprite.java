f9ae8685708b6dd1641748beca63a428247d37cf
Romario <boriso-rom@yandex.ru>
2023-12-02 17:51:32 +0300

    added code to sprite and created class ball

--- Lesson2/Sprite.java
+++ Lesson2/Sprite.java
@@ -1,4 +1,23 @@
 package Lesson2;
 
-public class Sprite {
+import java.awt.*;
+
+public abstract class Sprite {
+    protected float x;
+    protected float y;
+    protected float halfWidth;
+    protected float halfHeight;
+
+    protected  float getLeft() {return x - halfWidth;}
+    protected void setLeft(float left) {x = left + halfHeight;}
+    protected  float getRight() {return x + halfWidth;}
+    protected void setRight(float right) {x = right - halfWidth;}
+    protected float getTop() {return y - halfHeight;}
+    protected void setTop(float top) { y = top + halfHeight;}
+    protected float getBottom() {return y + halfHeight;}
+    protected void setBottom (float bottom) {y = bottom - halfHeight;}
+
+    void update (MainCanvas canvas, float deltaTime){}
+    void render(MainCanvas canvas, Graphics g) {}
 }
+
