99f632af7fadbf4fc02f5b018f109ad5aa5642f3
Roman Borisov <boriso-rom@yandex.ru>
2023-12-09 17:08:28 +0300

    added code to Pair and Main

--- PairClass/Main.java
+++ PairClass/Main.java
@@ -1,4 +1,12 @@
 package PairClass;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Pair<Integer, String> pair1 = new Pair<>(1, "One");
+        Pair<Double, Boolean> pair2 = new Pair<>(3.14, true);
+
+        System.out.println("Pair 1: " + pair1.toString());
+        System.out.println("Pair 2: " + pair2.toString());
+    }
 }
--- PairClass/Pair.java
+++ PairClass/Pair.java
@@ -1,4 +1,24 @@
 package PairClass;
 
-public class Pair {
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
 }
