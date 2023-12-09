23ae025b1b5f18903c8f398e516d1162a0b3f237
Roman Borisov <boriso-rom@yandex.ru>
2023-12-09 17:04:52 +0300

    added code

--- CompareArrays/ArrayComparer.java
+++ CompareArrays/ArrayComparer.java
@@ -1,4 +1,26 @@
 package CompareArrays;
 
 public class ArrayComparer {
+
+    public static <T> String compareArrays(T[] array1, T[] array2) {
+        if (array1 == null || array2 == null) {
+            throw new IllegalArgumentException("Один из массивов равен null.");
+        }
+
+        int lengthComparison = Integer.compare(array1.length, array2.length);
+
+        if (lengthComparison != 0) {
+            return "Массивы разной длинны";
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            int elementComparison = ((Comparable<T>) array1[i]).compareTo(array2[i]);
+            if (elementComparison != 0) {
+                return "Массивы различаются на позиции " + i + ".";
+            }
+        }
+
+        // Если массивы одинаковые
+        return "Массивы идентичны";
+    }
 }
--- CompareArrays/Main.java
+++ CompareArrays/Main.java
@@ -1,4 +1,19 @@
 package CompareArrays;
 
+import static CompareArrays.ArrayComparer.compareArrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Integer[] intArray1 = {1, 2, 3, 4, 5};
+        Integer[] intArray2 = {1, 2, 3, 4, 5, 6};
+        System.out.println("Сравнение массивов целых чисел: " + compareArrays(intArray1, intArray2));
+
+        String[] strArray1 = {"apple", "banana", "orange"};
+        String[] strArray2 = {"apple", "banana", "orange", "kiwi"};
+        System.out.println("Сравнение массивов строк: " + compareArrays(strArray1, strArray2));
+
+        Double[] doubleArray1 = {1.5, 2.0, 3.7};
+        Double[] doubleArray2 = {1.5, 2.0, 3.7};
+        System.out.println("Сравнение массивов вещественных чисел: " + compareArrays(doubleArray1, doubleArray2));
+    }
 }
