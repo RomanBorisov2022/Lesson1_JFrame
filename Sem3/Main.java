020a80d3b4e8add4f1259815ece12c903432fb2c
Roman Borisov <boriso-rom@yandex.ru>
2023-12-09 12:53:30 +0300

    changed code in main

--- Sem3/Main.java
+++ Sem3/Main.java
@@ -10,13 +10,13 @@ public class Main {
         Class1<String, DataInputStream, Integer> class1 = new Class1<>(t, v, k);
         class1.printTypes();
 
-/*        Number[] numArray = new Number[3];
+        Number[] numArray = new Number[3];
         numArray[0] = 1;
         numArray[1] = 2.0f;
         numArray[2] = 1565765876L;
         MyList<Number> myList = new MyList<>(numArray);
         myList.printList();
-
+        System.out.println("\n");
         Integer num = 5;
         myList.addElement(num);
         myList.printList();
@@ -26,8 +26,8 @@ public class Main {
         myList.removeElement(2);
         myList.printList();
         System.out.println("Length: " + myList.getArrayLength());
-        System.out.println("Size: " + myList.getSize());*/
-
+        System.out.println("Size: " + myList.getSize());
+        System.out.println("\n");
         MyList<Number> myList1 = new MyList<>();
         myList1.addElement(1);
         myList1.addElement(3);
