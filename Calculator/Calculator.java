aaa08c6379e5cadfc738f87b5783450323b5fd8c
Roman Borisov <boriso-rom@yandex.ru>
2023-12-09 16:53:15 +0300

    added code

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,4 +1,38 @@
 package Calculator;
 
 public class Calculator {
+
+    public static <T extends Number> T sum(T a, T b) {
+        if (a instanceof Integer) {
+            return (T) Integer.valueOf(a.intValue() + b.intValue());
+        } else if (a instanceof Double) {
+            return (T) Double.valueOf(a.doubleValue() + b.doubleValue());
+        }
+        throw new IllegalArgumentException("Неподдерживаемый тип данных");
+    }
+
+    public static <T extends Number> T multiply(T a, T b) {
+        if (a instanceof Integer) {
+            return (T) Integer.valueOf(a.intValue() * b.intValue());
+        } else if (a instanceof Double) {
+            return (T) Double.valueOf(a.doubleValue() * b.doubleValue());
+        }
+        throw new IllegalArgumentException("Неподдерживаемый тип данных");
+    }
+
+    public static <T extends Number> double divide(T a, T b) {
+        if (b.doubleValue() == 0) {
+            throw new IllegalArgumentException("Деление на ноль невозможно.");
+        }
+        return a.doubleValue() / b.doubleValue();
+    }
+
+    public static <T extends Number> T subtract(T a, T b) {
+        if (a instanceof Integer) {
+            return (T) Integer.valueOf(a.intValue() - b.intValue());
+        } else if (a instanceof Double) {
+            return (T) Double.valueOf(a.doubleValue() - b.doubleValue());
+        }
+        throw new IllegalArgumentException("Неподдерживаемый тип данных");
+    }
 }
--- Calculator/Main.java
+++ Calculator/Main.java
@@ -1,7 +1,60 @@
 package Calculator;
 
+import java.util.Scanner;
+
+import static Calculator.Calculator.divide;
+import static Calculator.Calculator.sum;
+import static Calculator.Calculator.multiply;
+import static Calculator.Calculator.subtract;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        boolean continueCalculations = true;
+
+        while (continueCalculations) {
+            System.out.println("Введите первое число:");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Введите второе число:");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Выберите операцию:");
+            System.out.println("1. Сложение");
+            System.out.println("2. Умножение");
+            System.out.println("3. Деление");
+            System.out.println("4. Вычитание");
+            System.out.println("5. Завершить работу");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Результат сложения: " + sum(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Результат умножения: " + multiply(num1, num2));
+                    break;
+                case 3:
+                    try {
+                        System.out.println("Результат деления: " + divide(num1, num2));
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    System.out.println("Результат вычитания: " + subtract(num1, num2));
+                    break;
+                case 5:
+                    continueCalculations = false;
+                    break;
+                default:
+                    System.out.println("Некорректный выбор операции.");
+            }
+        }
+
+        System.out.println("Работа программы завершена.");
+        scanner.close();
     }
 }
